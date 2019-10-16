a9e02c808b2c38e16069519d4b41fa865dd25d8a
Robert M Ricci <rmricci121@gmail.com>
2019-10-16 01:44:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,15 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 -num2
 end
 
 def division(num1, num2)
-
+  quotant = num2/num1
 end
 
 def multiplication(num1, num2)
-
+  
 end
 
 def modulo(num1, num2)
